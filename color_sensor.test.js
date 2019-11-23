ba897505498aa35655a6e83dc502a18fdb268d33
John Ryan <jryan@pivotal.io>
2019-11-22 23:27:56 -0800

    Generate color specs from data, not messages
    ... so that we can use the same output to generate the message and
    register an event listener
    
    Authored-by: John Ryan <jryan@pivotal.io>

--- color_sensor.test.js
+++ color_sensor.test.js
@@ -1,57 +1,72 @@
-var newColorSensor = function(getColor) {
-    function calcRange(colorValues) {
-       if (colorValues === undefined) {
-           return 'No color data has been recorded.  Did your program include a call to start recording?';
-       }
-       var results = {
-           r: {avg: colorValues[0].r, delta: 0},
-           g: {avg: colorValues[0].g, delta: 0},
-           b: {avg: colorValues[0].b, delta: 0}
-       };
-       return "red, " + results.r.avg + ", plus or minus " + results.r.delta + "; " +
-           "green, " + results.g.avg + ", plus or minus " + results.g.delta + "; " +
-           "blue, " + results.b.avg + ", plus or minus " + results.b.delta + ".";
+// newColorSensor returns a wrapper around the RVR color sensor
+//   getColor = the global `getColor()` RVR function
+var newColorSensor = function (getColor) {
+    function generateColorSpec(colorValues) {
+        if (colorValues === undefined) {
+            return {
+                r: {value: 0, tolerance: 0},
+                g: {value: 0, tolerance: 0},
+                b: {value: 0, tolerance: 0},
+            }
+        }
+        return {
+            r: {value: colorValues[0].r, tolerance: 0},
+            g: {value: colorValues[0].g, tolerance: 0},
+            b: {value: colorValues[0].b, tolerance: 0}
+        };
     }
 
     return {
-        calcRange: calcRange
+        generateColorSpec: generateColorSpec
     }
 };
 
 describe('colorSensor', () => {
     let colorSensor;
 
+    describe('when()', () => {
+        xtest('given a matcher', () => {
+            colorSensor = newColorSensor();
+            let blueSpec = {
+                r: {value: 0, tolerance: 0},
+                g: {value: 0, tolerance: 5},
+                b: {value: 100, tolerance: 50}
+            };
+            let colorMatched = false;
+
+            colorSensor.when(blueSpec, function () {
+                colorMatched = true;
+            });
+        })
+    });
+
     describe('startRecording()', () => {
     });
     describe('stopRecording()', () => {
     });
-    describe('calcRange()', () => {
-        test('given no data, returns "no data recorded" message.', () => {
+    describe('generateColorSpec()', () => {
+        test('given no data, returns black/off spec.', () => {
             colorSensor = newColorSensor();
-            colorRangeMsg = colorSensor.calcRange();
-            expect(colorRangeMsg).toBe('No color data has been recorded.  Did your program include a call to start recording?');
+            let spec = colorSensor.generateColorSpec();
+            expect(spec).toStrictEqual({
+                r: {value: 0, tolerance: 0},
+                g: {value: 0, tolerance: 0},
+                b: {value: 0, tolerance: 0}
+            });
         });
         test('given a single value, reports that value, including 0 plus/minuses', () => {
             let values = [
                 {r: 1, g: 10, b: 100}
             ];
             colorSensor = newColorSensor();
-            let colorRangeMsg = colorSensor.calcRange(values);
-            expect(colorRangeMsg).toBe('red, 1, plus or minus 0; green, 10, plus or minus 0; blue, 100, plus or minus 0.');
+            let spec = colorSensor.generateColorSpec(values);
+            expect(spec).toStrictEqual({
+                r: {value: 1, tolerance: 0},
+                g: {value: 10, tolerance: 0},
+                b: {value: 100, tolerance: 0}
+            });
         });
-        xtest('given a set of colors, reports the averages of each color value, including their deltas', () => {
-            let values = [
-                {r: 1, g: 10, b: 110},
-                {r: 2, g: 20, b: 120},
-                {r: 3, g: 30, b: 130},
-                {r: 4, g: 40, b: 140},
-                {r: 5, g: 40, b: 150},
-                {r: 6, g: 50, b: 160}
-            ];
-            colorSensor = newColorSensor();
-            let colorRangeMsg = colorSensor.calcRange(values);
-            expect(colorRangeMsg).toBe('red, 3, plus or minus 0; green, 10, plus or minus 0; blue, 100, plus or minus 0.');
-        });
+        test.todo('given a set of values, returns the average values and tolerance');
     });
 });
 
